83a8bb94ea9796e3e5cc49970737250d127cf3bb
Kauê Rafael <94557995+Kaue0@users.noreply.github.com>
2023-12-01 11:11:09 -0300

    Update CorsConfigs.java

--- safelearn/backend/src/main/java/com/safelearn/backend/config/CorsConfigs.java
+++ safelearn/backend/src/main/java/com/safelearn/backend/config/CorsConfigs.java
@@ -10,9 +10,9 @@ public class CorsConfigs implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5173", "https://safelearn.netlify.app/", "http://localhost:8080, https://safelearn.up.railway.app/,
-http://safelearn.eastus.cloudapp.azure.com:3000,
-https://safelearn.eastus.cloudapp.azure.com:3000")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5173", "https://safelearn.netlify.app/", "http://localhost:8080", "https://safelearn.up.railway.app/",
+"http://safelearn.eastus.cloudapp.azure.com:3000",
+"https://safelearn.eastus.cloudapp.azure.com:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*");
     }
